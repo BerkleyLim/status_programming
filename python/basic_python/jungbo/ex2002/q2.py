cbf0890a4569dd1b727b280ab7b48cdd38f60808
berkleylim <berkleylim16@gmail.com>
2023-04-07 15:18:15 +0900

    test: 정처기 기출 테스트

---
+++ python/basic_python/jungbo/ex2002/q2.py
@@ -0,0 +1,12 @@
+asia={"한국", "중국", "일본"}
+asia.add("베트남")
+asia.add("중국")
+asia.remove("일본")
+asia.update(["홍콩","한국","태국"])
+print(asia)
+
+# 답 : 순서 무관하게 출력
+# 소괄호 - 튜플
+# 중괄호 - 딕셔너리,set
+# 대괄호 - 리스트
+# update - set 두개를 통합
